e775980ee02be6ded86612499b4e9bfa3c5938e3
Andres Piedrahita <andresfph1989@gmail.com>
2017-04-19 16:08:22 +0200

    Add Language model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419130405) do
+ActiveRecord::Schema.define(version: 20170419140137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20170419130405) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "languages", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.string   "name"
+    t.string   "level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
---
+++ spec/factories/languages.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: languages
+#
+#  id         :uuid             not null, primary key
+#  name       :string
+#  level      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+FactoryGirl.define do
+  factory :language do
+    name  'English'
+    level 'A1'
+  end
+end
