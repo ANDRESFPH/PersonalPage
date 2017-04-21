069c21d0188a1e110bbdac5f103f20f13abeb3ff
Andres Piedrahita <andresfph1989@gmail.com>
2017-04-21 12:20:29 +0200

    Add Education model and change configuration of rubocop to exclude bolock lenght in specs

---
+++ app/models/education.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: educations
+#
+#  id              :uuid             not null, primary key
+#  university_name :string
+#  start_date      :datetime
+#  end_date        :datetime
+#  title           :string
+#  degree          :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class Education < ApplicationRecord
+  validates :university_name, presence: true
+  validates :start_date, presence: true
+  validates :title, presence: true
+  validates :degree, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419141425) do
+ActiveRecord::Schema.define(version: 20170419143146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 20170419141425) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "educations", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.string   "university_name"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string   "title"
+    t.string   "degree"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "languages", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
